ed021e7b3b15ccc813c7848ec6b5c9376bc20cff
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2020-09-11 15:12:51 +0200

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+Ingredient.destroy_all
+
+ingredients = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read
+drink_ingredients = JSON.parse(ingredients)
+
+drink_ingredients["drinks"].each do |ing|
+  Ingredient.create(name: ing["strIngredient1"])
+end
+
+print "Added "
+print Ingredient.count
+print " ingredients"
